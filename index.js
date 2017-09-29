9fa5976175cf43e22a5053ad0c4ca29f295f6c5c
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-29 10:25:25 +0700

    Remove async/await, simple pure chain

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-// const log = console.log
-// const injectPage = page => callback => {console.log(page); return callback}
-// const doAction = action => console.log(action)
-//
-// injectPage({page: "pageA"})(doAction)({action: "goToUrl"})
-//
-// var attitude = (original, replacement) => source => source.replace(original, replacement)
-// var snakify = attitude(/millenials/ig, "Snake People");
-// var hippify = attitude(/baby boomers/ig, "Aging Hippies");
-//
-// console.log(snakify("The Millenials are always up to something."));
-
 const storeResult = lastResult => callback => {
-  const callback2 = async (args) => {
-    const result = await callback(args);
+  const callback2 = (args) => {
+    const result = callback(args);
     return Object.assign({}, lastResult, result)
   }
   return callback2
 }
 
-const doX = async action => {return action}
+const doX = action => action
 
-const run = async () => {
-  const result = await storeResult({last: "anh"})(doX)({action: "click"})
+const run = () => {
+  const result = storeResult({last: "anh"})(doX)({action: "click"})
   console.log(result)
 }
 
