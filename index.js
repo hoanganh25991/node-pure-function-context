4a807873233ef1daf3c65166a17d0ea836e584a2
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-29 10:56:46 +0700

    Pure function should PURE, self callback not good

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-const storeResult = lastResult => callback => {
-  const callback2 = (args) => {
-    const result = callback(args);
-    return Object.assign({}, lastResult, result)
-  }
-  return callback2
+const isFunction = (functionToCheck) => {
+  var getType = {};
+  return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
 }
+
+const StoreResult = () => {
+  const lastResult = {}
+  return {
+    store(result){
+      Object.assign(lastResult, result)
+    },
+    get(){
+      return lastResult
+    }
+  }
+}
+
+const s = StoreResult();
 
 const doX = action => action
 
+
+//=============================================
+// inject by pure function
+//=============================================
+const doPageX = page => action => ({page, action})
+const doPageXNeedResult = lastResult => {
+  console.log(lastResult)
+  return doPageX
+}
+
 const run = () => {
-  const result = storeResult({last: "anh"})(doX)({action: "click"})
-  console.log(result)
+  const page = {page: "pageX"}
+  const action = {action: "actionX"}
+  s.store(doPageX(page)(action))
+  console.log(s.get())
 }
 
 run()
